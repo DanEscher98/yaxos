f598c583a940622d9e79cb05e4263221ba9e37b7
Danyiel Colin <danyiel5978@gmail.com>
2023-06-13 23:37:55 -0600

    print Hello World: 2023:06:13

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_std]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -12,13 +13,6 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World, {}!", "Daniel");
     loop {}
 }
